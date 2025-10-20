84080295afff0a61789fab3eb7d848e571a6c7ea
Aaron-IV <Mahaizi@yandex.ru>
2025-10-20 19:06:17 +0500

    fe

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	fmt.Scan(&a) // считаем переменную 'a' с консоли
+	fmt.Scan(&b) // считаем переменную 'b' с консоли
+
+	a = a * a
+	b = b * 2
+	c = a + b
+	fmt.Println(c)
+}
